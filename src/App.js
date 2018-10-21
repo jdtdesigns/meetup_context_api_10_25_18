8fce419c1757d9f08d2d5c27bc591dcedd5bde37
JD <jt7903@gmail.com>
2018-10-21 12:19:14 -0400

    redux fin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateTitle } from './';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <h1>{this.props.title}</h1>
+
+        <input type="text" value={this.props.title} onChange={this.props.handleChange} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  title: state.title
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleChange(e) {
+    dispatch(updateTitle(e.target.value));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Types
+const UPDATE_TITLE = 'UPDATE_TITLE';
+
+// Actions
+export const updateTitle = payload => ({
+  type: UPDATE_TITLE,
+  payload
+});
+
+const initial_state = {
+  title: 'Redux Example'
+}
+
+
+const reducer = (state = initial_state , {type, payload}) => {
+  switch(type) {
+    case UPDATE_TITLE:
+      return {...state, title: payload}
+    default: return state;
+  }
+}
+
+const store = createStore(reducer);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
