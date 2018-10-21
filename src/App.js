7a0d62177a83876d6db018c881006615855cd970
JD <jt7903@gmail.com>
2018-10-21 11:58:44 -0400

    standard fin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-class App extends Component {
+const Student = props => (
+  <Fragment>
+    <h4>I attend the bootcamp at {props.uni_name}</h4>
+
+    <label>
+      Update Name:
+      <input type="text" name="uni_name" value={props.uni_name} onChange={props.handleChange} />
+    </label>
+  </Fragment>
+);
+
+const Class = props => (
+  <Fragment>
+    <h3>December class at {props.uni_name}</h3>  
+
+    <Student uni_name={props.uni_name} handleChange={props.handleChange} />
+  </Fragment>
+);
+
+class University extends Component {
+  state = {
+    uni_name: 'Georgia Tech'
+  }
+
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <Fragment>
+        <h1>Welcome to {this.state.uni_name}</h1>
+
+        <Class uni_name={this.state.uni_name} handleChange={this.handleChange} />
+      </Fragment>
     );
   }
 }
 
-export default App;
+export default University;
