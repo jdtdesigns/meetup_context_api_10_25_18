fd6e9b5f5047d9ed3efe6853db0bbfe9646fa49e
JD <jt7903@gmail.com>
2018-10-23 13:03:32 -0400

    updated walkthrough order

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 
 // Redux
+// #6
 import { connect } from 'react-redux';
 import { updateUniversity, getJoke } from './';
 
@@ -30,7 +31,9 @@ const Class = connect(
   <Fragment>
     <h3>December class at {props.uni_name}</h3>
     
-    {props.loading ? <i className="fa fa-spinner fa-spin"></i> : <p dangerouslySetInnerHTML={{ __html: props.joke }}></p>}
+    {props.loading ? 
+      <i className="fa fa-spinner fa-spin"></i> : 
+      <p dangerouslySetInnerHTML={{ __html: props.joke }}></p>}
 
     <Student />
   </Fragment>
@@ -53,16 +56,22 @@ class University extends Component {
   }
 }
 
-// #6-A -- Share properties from the global state with the component
+// #7-A -- Share properties from the global state with the component
 const mapStateToProps = state => ({
   uni_name: state.uni_name
 });
 
-// #6-B -- connect actions and/or dispatches with the component
+// #7-B -- connect actions and/or dispatches with the component
 const mapDispatchesToProps = {
   getJoke
 };
 
-// #6 -- Connect component to store
+// const mapDispatchesToProps = dispatch => ({
+//   handleChange: e => {
+//     dispatch(updateUniversity(e.target.value));
+//   }
+// });
+
+// #7 -- Connect component to store
 export default connect(mapStateToProps, mapDispatchesToProps)(University);
 
