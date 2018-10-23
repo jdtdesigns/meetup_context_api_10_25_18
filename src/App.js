f427254b72fea1ecff38eb769e0c0748817c3858
JD <jt7903@gmail.com>
2018-10-22 21:16:41 -0400

    added fontawesome and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         
-      </div>
+      </Fragment>
     );
   }
 }
